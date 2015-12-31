53dc8b6c118220fb2eabaf5084be2e80c1150f96
tmngassa@hotmail.com <tmngassa@hotmail.com>
2015-12-31 12:12:39 -0500

    Add angular2 quickstart app with TS

---
+++ quickstart-ts/app/app.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by tomsonngassa on 12/27/15.
+ */
+
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { }
